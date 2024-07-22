40e43c2a77fbb03950ff6217b0298948cf90ad5f
Jannik Stehle <jannik.stehle@kiteworks.com>
2024-07-22 13:44:06 +0200

    feat: add draw-io extension

---
+++ packages/web-app-draw-io/vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from '@ownclouders/extension-sdk'
+
+export default defineConfig({
+  name: 'web-app-draw-io',
+  server: {
+    port: 9724
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'draw-io.js'
+      }
+    }
+  }
+})
